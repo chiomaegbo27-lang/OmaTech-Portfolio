3b4862a6f380938f4667441475c9e1631ab283ec
Chioma Egbo <chiomaegbo27@gmail.com>
2026-01-17 07:35:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Full-Stack Web Developer", "Virtual Assistant", "Data Analyst", "YouTuber", "Tech Coach/Tutor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
